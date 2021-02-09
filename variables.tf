96e99859ced4bd05cb2aa89ed7e9daad1e214470
Kyle Smith <kyle.r.smith@accenture.com>
2021-02-09 15:57:33 -0500

    Common#512 run terraform 0.12upgrade on repo

--- variables.tf
+++ variables.tf
@@ -34,48 +34,48 @@ variable "manage_aws_auth" {
 
 variable "map_accounts" {
   description = "Additional AWS account numbers to add to the aws-auth configmap. See examples/eks_test_fixture/variables.tf for example format."
-  type        = "list"
+  type        = list(string)
   default     = []
 }
 
 variable "map_accounts_count" {
   description = "The count of accounts in the map_accounts list."
-  type        = "string"
+  type        = string
   default     = 0
 }
 
 variable "map_roles" {
   description = "Additional IAM roles to add to the aws-auth configmap. See examples/eks_test_fixture/variables.tf for example format."
-  type        = "list"
+  type        = list(string)
   default     = []
 }
 
 variable "map_roles_count" {
   description = "The count of roles in the map_roles list."
-  type        = "string"
+  type        = string
   default     = 0
 }
 
 variable "map_users" {
   description = "Additional IAM users to add to the aws-auth configmap. See examples/eks_test_fixture/variables.tf for example format."
-  type        = "list"
+  type        = list(string)
   default     = []
 }
 
 variable "map_users_count" {
   description = "The count of roles in the map_users list."
-  type        = "string"
+  type        = string
   default     = 0
 }
 
 variable "subnets" {
   description = "A list of subnets to place the EKS cluster and workers within."
-  type        = "list"
+  type        = list(string)
 }
 
 variable "tags" {
   description = "A map of tags to add to all resources."
-  type        = "map"
+  type        = map(string)
   default     = {}
 }
 
@@ -85,7 +85,7 @@ variable "vpc_id" {
 
 variable "worker_groups" {
   description = "A list of maps defining worker group configurations to be defined using AWS Launch Configurations. See workers_group_defaults for valid keys."
-  type        = "list"
+  type        = list(string)
 
   default = [
     {
@@ -96,19 +96,19 @@ variable "worker_groups" {
 
 variable "worker_group_count" {
   description = "The number of maps contained within the worker_groups list."
-  type        = "string"
+  type        = string
   default     = "1"
 }
 
 variable "workers_group_defaults" {
   description = "Override default values for target groups. See workers_group_defaults_defaults in locals.tf for valid keys."
-  type        = "map"
+  type        = map(string)
   default     = {}
 }
 
 variable "worker_groups_launch_template" {
   description = "A list of maps defining worker group configurations to be defined using AWS Launch Templates. See workers_group_defaults for valid keys."
-  type        = "list"
+  type        = list(string)
 
   default = [
     {
@@ -119,13 +119,13 @@ variable "worker_groups_launch_template" {
 
 variable "worker_group_launch_template_count" {
   description = "The number of maps contained within the worker_groups_launch_template list."
-  type        = "string"
+  type        = string
   default     = "0"
 }
 
 variable "workers_group_launch_template_defaults" {
   description = "Override default values for target groups. See workers_group_defaults_defaults in locals.tf for valid keys."
-  type        = "map"
+  type        = map(string)
   default     = {}
 }
 
@@ -136,7 +136,7 @@ variable "worker_security_group_id" {
 
 variable "worker_additional_security_group_ids" {
   description = "A list of additional security group ids to attach to worker instances"
-  type        = "list"
+  type        = list(string)
   default     = []
 }
 
@@ -152,19 +152,19 @@ variable "kubeconfig_aws_authenticator_command" {
 
 variable "kubeconfig_aws_authenticator_command_args" {
   description = "Default arguments passed to the authenticator command. Defaults to [token -i $cluster_name]."
-  type        = "list"
+  type        = list(string)
   default     = []
 }
 
 variable "kubeconfig_aws_authenticator_additional_args" {
   description = "Any additional arguments to pass to the authenticator such as the role to assume. e.g. [\"-r\", \"MyEksRole\"]."
-  type        = "list"
+  type        = list(string)
   default     = []
 }
 
 variable "kubeconfig_aws_authenticator_env_variables" {
   description = "Environment variables that should be used when executing the authenticator. e.g. { AWS_PROFILE = \"eks\"}."
-  type        = "map"
+  type        = map(string)
   default     = {}
 }
 
@@ -185,7 +185,7 @@ variable "cluster_delete_timeout" {
 
 variable "local_exec_interpreter" {
   description = "Command to run for local-exec resources. Must be a shell-style interpreter. If you are on Windows Git Bash is a good choice."
-  type        = "list"
+  type        = list(string)
   default     = ["/bin/sh", "-c"]
 }
 
@@ -198,3 +198,4 @@ variable "worker_create_security_group" {
   description = "Whether to create a security group for the workers or attach the workers to `worker_security_group_id`."
   default     = true
 }
+
--- workers.tf
+++ workers.tf
@@ -1,6 +1,10 @@
 resource "aws_cloudformation_stack" "workers" {
-  count         = "${var.worker_group_count}"
-  name          = "${replace("${aws_eks_cluster.this.name}-${lookup(var.worker_groups[count.index], "name", count.index)}", "/[^-a-zA-Z0-9]/", "-")}"
+  count = var.worker_group_count
+  name = replace(
+    "${aws_eks_cluster.this.name}-${lookup(var.worker_groups[count.index], "name", count.index)}",
+    "/[^-a-zA-Z0-9]/",
+    "-",
+  )
   template_body = <<EOF
 ---
 AWSTemplateFormatVersion: "2010-09-09"
@@ -10,20 +14,55 @@ Resources:
     Type: AWS::AutoScaling::AutoScalingGroup
     Properties:
       AutoScalingGroupName: ${aws_eks_cluster.this.name}-${lookup(var.worker_groups[count.index], "name", count.index)}
-      VPCZoneIdentifier: ${jsonencode(split(",", coalesce(lookup(var.worker_groups[count.index], "subnets", ""), join(",", var.subnets))))}
+      VPCZoneIdentifier: ${jsonencode(
+  split(
+    ",",
+    coalesce(
+      lookup(var.worker_groups[count.index], "subnets", ""),
+      join(",", var.subnets),
+    ),
+  ),
+  )}
       LaunchConfigurationName: ${element(aws_launch_configuration.workers.*.id, count.index)}
-      MinSize: ${lookup(var.worker_groups[count.index], "asg_min_size",lookup(local.workers_group_defaults, "asg_min_size"))}
-      MaxSize: ${lookup(var.worker_groups[count.index], "asg_max_size",lookup(local.workers_group_defaults, "asg_max_size"))}
-      Tags: ${jsonencode(concat(
-        list(
-          map("Key", "Name", "Value", "${aws_eks_cluster.this.name}-${lookup(var.worker_groups[count.index], "name", count.index)}-eks_asg", "PropagateAtLaunch", "True"),
-          map("Key", "kubernetes.io/cluster/${aws_eks_cluster.this.name}", "Value", "owned", "PropagateAtLaunch", "True"),
-        ),
-        local.asg_tags))}
+      MinSize: ${lookup(
+  var.worker_groups[count.index],
+  "asg_min_size",
+  local.workers_group_defaults["asg_min_size"],
+  )}
+      MaxSize: ${lookup(
+  var.worker_groups[count.index],
+  "asg_max_size",
+  local.workers_group_defaults["asg_max_size"],
+  )}
+      Tags: ${jsonencode(
+  concat(
+    [
+      {
+        "Key"               = "Name"
+        "Value"             = "${aws_eks_cluster.this.name}-${lookup(var.worker_groups[count.index], "name", count.index)}-eks_asg"
+        "PropagateAtLaunch" = "True"
+      },
+      {
+        "Key"               = "kubernetes.io/cluster/${aws_eks_cluster.this.name}"
+        "Value"             = "owned"
+        "PropagateAtLaunch" = "True"
+      },
+    ],
+    local.asg_tags,
+  ),
+  )}
     UpdatePolicy:
       AutoScalingRollingUpdate:
-        MaxBatchSize: ${lookup(var.worker_groups[count.index], "asg_rolling_update_max_batch_size",lookup(local.workers_group_defaults, "asg_rolling_update_max_batch_size"))}
-        MinInstancesInService: ${lookup(var.worker_groups[count.index], "asg_rolling_update_min_instances_in_service",lookup(local.workers_group_defaults, "asg_rolling_update_min_instances_in_service"))}
+        MaxBatchSize: ${lookup(
+  var.worker_groups[count.index],
+  "asg_rolling_update_max_batch_size",
+  local.workers_group_defaults["asg_rolling_update_max_batch_size"],
+  )}
+        MinInstancesInService: ${lookup(
+  var.worker_groups[count.index],
+  "asg_rolling_update_min_instances_in_service",
+  local.workers_group_defaults["asg_rolling_update_min_instances_in_service"],
+)}
         SuspendProcesses:
           - HealthCheck
           - ReplaceUnhealthy
@@ -35,139 +74,210 @@ Outputs:
     Description: The name of the Auto-Scaling Group
     Value: !Ref AutoScalingGroup
 EOF
+
 }
 
 data "template_file" "workers_names" {
-  count    = "${var.worker_group_count}"
-  template = "${lookup(aws_cloudformation_stack.workers.*.outputs[count.index], "AutoScalingGroupName")}"
+  count    = var.worker_group_count
+  template = aws_cloudformation_stack.workers[count.index].outputs["AutoScalingGroupName"]
 }
 
 resource "aws_launch_configuration" "workers" {
-  name_prefix                 = "${aws_eks_cluster.this.name}-${lookup(var.worker_groups[count.index], "name", count.index)}"
-  associate_public_ip_address = "${lookup(var.worker_groups[count.index], "public_ip", local.workers_group_defaults["public_ip"])}"
-  security_groups             = ["${local.worker_security_group_id}", "${var.worker_additional_security_group_ids}", "${compact(split(",",lookup(var.worker_groups[count.index],"additional_security_group_ids", local.workers_group_defaults["additional_security_group_ids"])))}"]
-  iam_instance_profile        = "${element(aws_iam_instance_profile.workers.*.id, count.index)}"
-  image_id                    = "${lookup(var.worker_groups[count.index], "ami_id", local.workers_group_defaults["ami_id"])}"
-  instance_type               = "${lookup(var.worker_groups[count.index], "instance_type", local.workers_group_defaults["instance_type"])}"
-  key_name                    = "${lookup(var.worker_groups[count.index], "key_name", local.workers_group_defaults["key_name"])}"
-  user_data_base64            = "${base64encode(element(data.template_file.userdata.*.rendered, count.index))}"
-  ebs_optimized               = "${lookup(var.worker_groups[count.index], "ebs_optimized", lookup(local.ebs_optimized, lookup(var.worker_groups[count.index], "instance_type", local.workers_group_defaults["instance_type"]), false))}"
-  enable_monitoring           = "${lookup(var.worker_groups[count.index], "enable_monitoring", local.workers_group_defaults["enable_monitoring"])}"
-  spot_price                  = "${lookup(var.worker_groups[count.index], "spot_price", local.workers_group_defaults["spot_price"])}"
-  placement_tenancy           = "${lookup(var.worker_groups[count.index], "placement_tenancy", local.workers_group_defaults["placement_tenancy"])}"
-  count                       = "${var.worker_group_count}"
+  name_prefix = "${aws_eks_cluster.this.name}-${lookup(var.worker_groups[count.index], "name", count.index)}"
+  associate_public_ip_address = lookup(
+    var.worker_groups[count.index],
+    "public_ip",
+    local.workers_group_defaults["public_ip"],
+  )
+  security_groups = [local.worker_security_group_id, var.worker_additional_security_group_ids, compact(
+    split(
+      ",",
+      lookup(
+        var.worker_groups[count.index],
+        "additional_security_group_ids",
+        local.workers_group_defaults["additional_security_group_ids"],
+      ),
+    ),
+  )]
+  iam_instance_profile = element(aws_iam_instance_profile.workers.*.id, count.index)
+  image_id = lookup(
+    var.worker_groups[count.index],
+    "ami_id",
+    local.workers_group_defaults["ami_id"],
+  )
+  instance_type = lookup(
+    var.worker_groups[count.index],
+    "instance_type",
+    local.workers_group_defaults["instance_type"],
+  )
+  key_name = lookup(
+    var.worker_groups[count.index],
+    "key_name",
+    local.workers_group_defaults["key_name"],
+  )
+  user_data_base64 = base64encode(element(data.template_file.userdata.*.rendered, count.index))
+  ebs_optimized = lookup(
+    var.worker_groups[count.index],
+    "ebs_optimized",
+    lookup(
+      local.ebs_optimized,
+      lookup(
+        var.worker_groups[count.index],
+        "instance_type",
+        local.workers_group_defaults["instance_type"],
+      ),
+      false,
+    ),
+  )
+  enable_monitoring = lookup(
+    var.worker_groups[count.index],
+    "enable_monitoring",
+    local.workers_group_defaults["enable_monitoring"],
+  )
+  spot_price = lookup(
+    var.worker_groups[count.index],
+    "spot_price",
+    local.workers_group_defaults["spot_price"],
+  )
+  placement_tenancy = lookup(
+    var.worker_groups[count.index],
+    "placement_tenancy",
+    local.workers_group_defaults["placement_tenancy"],
+  )
+  count = var.worker_group_count
 
   lifecycle {
     create_before_destroy = true
   }
 
   root_block_device {
-    volume_size           = "${lookup(var.worker_groups[count.index], "root_volume_size", local.workers_group_defaults["root_volume_size"])}"
-    volume_type           = "${lookup(var.worker_groups[count.index], "root_volume_type", local.workers_group_defaults["root_volume_type"])}"
-    iops                  = "${lookup(var.worker_groups[count.index], "root_iops", local.workers_group_defaults["root_iops"])}"
+    volume_size = lookup(
+      var.worker_groups[count.index],
+      "root_volume_size",
+      local.workers_group_defaults["root_volume_size"],
+    )
+    volume_type = lookup(
+      var.worker_groups[count.index],
+      "root_volume_type",
+      local.workers_group_defaults["root_volume_type"],
+    )
+    iops = lookup(
+      var.worker_groups[count.index],
+      "root_iops",
+      local.workers_group_defaults["root_iops"],
+    )
     delete_on_termination = true
   }
 }
 
 resource "aws_security_group" "workers" {
-  name_prefix = "${aws_eks_cluster.this.name}"
+  name_prefix = aws_eks_cluster.this.name
   description = "Security group for all nodes in the cluster."
-  vpc_id      = "${var.vpc_id}"
-  count       = "${var.worker_create_security_group ? 1 : 0}"
-  tags        = "${merge(var.tags, map("Name", "${aws_eks_cluster.this.name}-eks_worker_sg", "kubernetes.io/cluster/${aws_eks_cluster.this.name}", "owned"
-  ))}"
+  vpc_id      = var.vpc_id
+  count       = var.worker_create_security_group ? 1 : 0
+  tags = merge(
+    var.tags,
+    {
+      "Name"                                               = "${aws_eks_cluster.this.name}-eks_worker_sg"
+      "kubernetes.io/cluster/${aws_eks_cluster.this.name}" = "owned"
+    },
+  )
 }
 
 resource "aws_security_group_rule" "workers_egress_internet" {
   description       = "Allow nodes all egress to the Internet."
   protocol          = "-1"
-  security_group_id = "${aws_security_group.workers.id}"
+  security_group_id = aws_security_group.workers[0].id
   cidr_blocks       = ["0.0.0.0/0"]
   from_port         = 0
   to_port           = 0
   type              = "egress"
-  count             = "${var.worker_create_security_group ? 1 : 0}"
+  count             = var.worker_create_security_group ? 1 : 0
 }
 
 resource "aws_security_group_rule" "workers_ingress_self" {
   description              = "Allow node to communicate with each other."
   protocol                 = "-1"
-  security_group_id        = "${aws_security_group.workers.id}"
-  source_security_group_id = "${aws_security_group.workers.id}"
+  security_group_id        = aws_security_group.workers[0].id
+  source_security_group_id = aws_security_group.workers[0].id
   from_port                = 0
   to_port                  = 65535
   type                     = "ingress"
-  count                    = "${var.worker_create_security_group ? 1 : 0}"
+  count                    = var.worker_create_security_group ? 1 : 0
 }
 
 resource "aws_security_group_rule" "workers_ingress_cluster" {
   description              = "Allow workers Kubelets and pods to receive communication from the cluster control plane."
   protocol                 = "tcp"
-  security_group_id        = "${aws_security_group.workers.id}"
-  source_security_group_id = "${local.cluster_security_group_id}"
-  from_port                = "${var.worker_sg_ingress_from_port}"
+  security_group_id        = aws_security_group.workers[0].id
+  source_security_group_id = local.cluster_security_group_id
+  from_port                = var.worker_sg_ingress_from_port
   to_port                  = 65535
   type                     = "ingress"
-  count                    = "${var.worker_create_security_group ? 1 : 0}"
+  count                    = var.worker_create_security_group ? 1 : 0
 }
 
 resource "aws_security_group_rule" "workers_ingress_cluster_https" {
   description              = "Allow pods running extension API servers on port 443 to receive communication from cluster control plane."
   protocol                 = "tcp"
-  security_group_id        = "${aws_security_group.workers.id}"
-  source_security_group_id = "${local.cluster_security_group_id}"
+  security_group_id        = aws_security_group.workers[0].id
+  source_security_group_id = local.cluster_security_group_id
   from_port                = 443
   to_port                  = 443
   type                     = "ingress"
-  count                    = "${var.worker_create_security_group ? 1 : 0}"
+  count                    = var.worker_create_security_group ? 1 : 0
 }
 
 resource "aws_iam_role" "workers" {
-  name_prefix           = "${aws_eks_cluster.this.name}"
-  assume_role_policy    = "${data.aws_iam_policy_document.workers_assume_role_policy.json}"
+  name_prefix           = aws_eks_cluster.this.name
+  assume_role_policy    = data.aws_iam_policy_document.workers_assume_role_policy.json
   force_detach_policies = true
 }
 
 resource "aws_iam_instance_profile" "workers" {
-  name_prefix = "${aws_eks_cluster.this.name}"
-  role        = "${lookup(var.worker_groups[count.index], "iam_role_id",  lookup(local.workers_group_defaults, "iam_role_id"))}"
-  count       = "${var.worker_group_count}"
+  name_prefix = aws_eks_cluster.this.name
+  role = lookup(
+    var.worker_groups[count.index],
+    "iam_role_id",
+    local.workers_group_defaults["iam_role_id"],
+  )
+  count = var.worker_group_count
 }
 
 resource "aws_iam_role_policy_attachment" "workers_AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = "${aws_iam_role.workers.name}"
+  role       = aws_iam_role.workers.name
 }
 
 resource "aws_iam_role_policy_attachment" "workers_AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = "${aws_iam_role.workers.name}"
+  role       = aws_iam_role.workers.name
 }
 
 resource "aws_iam_role_policy_attachment" "workers_AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = "${aws_iam_role.workers.name}"
+  role       = aws_iam_role.workers.name
 }
 
 resource "null_resource" "tags_as_list_of_maps" {
-  count = "${length(keys(var.tags))}"
+  count = length(keys(var.tags))
 
-  triggers = "${map(
-    "Key", "${element(keys(var.tags), count.index)}",
-    "Value", "${element(values(var.tags), count.index)}",
-    "PropagateAtLaunch", "True"
-  )}"
+  triggers = {
+    "Key"               = element(keys(var.tags), count.index)
+    "Value"             = element(values(var.tags), count.index)
+    "PropagateAtLaunch" = "True"
+  }
 }
 
 resource "aws_iam_role_policy_attachment" "workers_autoscaling" {
-  policy_arn = "${aws_iam_policy.worker_autoscaling.arn}"
-  role       = "${aws_iam_role.workers.name}"
+  policy_arn = aws_iam_policy.worker_autoscaling.arn
+  role       = aws_iam_role.workers.name
 }
 
 resource "aws_iam_policy" "worker_autoscaling" {
   name_prefix = "eks-worker-autoscaling-${aws_eks_cluster.this.name}"
   description = "EKS worker node autoscaling policy for cluster ${aws_eks_cluster.this.name}"
-  policy      = "${data.aws_iam_policy_document.worker_autoscaling.json}"
+  policy      = data.aws_iam_policy_document.worker_autoscaling.json
 }
 
 data "aws_iam_policy_document" "worker_autoscaling" {
@@ -210,3 +320,4 @@ data "aws_iam_policy_document" "worker_autoscaling" {
     }
   }
 }
+
